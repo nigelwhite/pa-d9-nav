a905c4cd3ab729f5a0b80704bff3cb986f87e59b
nigelwhite <webdev@permaculture.org.uk>
2022-02-02 16:28:20 +0000

    favourite starter

---
+++ web/sites/default/files/php/twig/61faad977d0ba_input--checkbox.html.twig_56c6GyiBVYwlFzgPblhF-HWLI/p57a9ePLI936rNO4O3FcdoELyT4Sdbxkr33sls1LvK4.php
@@ -0,0 +1,120 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/gin/templates/form/input--checkbox.html.twig */
+class __TwigTemplate_c196f6b1d2cabe8519fdf320c63d94f1c24374576a5362bf740a19a034826459 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        ob_start(function () { return ''; });
+        // line 15
+        if (($context["autocomplete_message"] ?? null)) {
+            // line 16
+            echo "  <div class=\"claro-autocomplete\">
+    <input";
+            // line 17
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 17, $this->source), "html", null, true);
+            echo "/>
+    <div hidden class=\"claro-autocomplete__message\">";
+            // line 18
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["autocomplete_message"] ?? null), 18, $this->source), "html", null, true);
+            echo "</div>
+  </div>
+  ";
+            // line 20
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 20, $this->source), "html", null, true);
+            echo "
+";
+        } else {
+            // line 22
+            echo "  <input";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 22, $this->source), "html", null, true);
+            echo "/>";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 22, $this->source), "html", null, true);
+            echo "
+  <span class=\"checkbox-toggle\">
+    <span class=\"checkbox-toggle__inner\"></span>
+  </span>
+";
+        }
+        echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/gin/templates/form/input--checkbox.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  60 => 22,  55 => 20,  50 => 18,  46 => 17,  43 => 16,  41 => 15,  39 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/gin/templates/form/input--checkbox.html.twig", "/var/www/html/web/themes/contrib/gin/templates/form/input--checkbox.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("spaceless" => 14, "if" => 15);
+        static $filters = array("escape" => 17);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['spaceless', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/61faad977d0ba_toolbar--gin--secondary.h_cKvO4IGk88ZWN0TH0r3oBtJkZ/vDJiQkRkR5Qlv7ybOdL46JakuAivA3gGD2cbnTFa3lg.php
@@ -0,0 +1,231 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/gin/templates/navigation/toolbar--gin--secondary.html.twig */
+class __TwigTemplate_e39951e9a282adc85f90b4dbe656450b2e4bfda409ff1969f28063214f33b76a extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 23
+        echo "<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "toolbar", 1 => "toolbar-meta"], "method", false, false, true, 23), 23, $this->source), "html", null, true);
+        echo ">
+  <nav";
+        // line 24
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["toolbar_attributes"] ?? null), "addClass", [0 => "toolbar-bar", 1 => "clearfix"], "method", false, false, true, 24), 24, $this->source), "html", null, true);
+        echo ">
+    <h2 class=\"visually-hidden\">";
+        // line 25
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["toolbar_heading"] ?? null), 25, $this->source), "html", null, true);
+        echo "</h2>
+
+    ";
+        // line 27
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["tabs"] ?? null));
+        foreach ($context['_seq'] as $context["key"] => $context["tab"]) {
+            // line 28
+            echo "      ";
+            $context["tray"] = (($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 = ($context["trays"] ?? null)) && is_array($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4) || $__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 instanceof ArrayAccess ? ($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4[$context["key"]] ?? null) : null);
+            // line 29
+            echo "      ";
+            $context["user_menu"] = (((($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 = twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "links", [], "any", false, false, true, 29)) && is_array($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144) || $__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 instanceof ArrayAccess ? ($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144["user_links"] ?? null) : null)) ? ("user-menu") : (false));
+            // line 30
+            echo "      ";
+            $context["item_id"] = [];
+            // line 31
+            echo "
+      ";
+            // line 32
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b = (($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002 = twig_get_attribute($this->env, $this->source, $context["tab"], "link", [], "any", false, false, true, 32)) && is_array($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002) || $__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002 instanceof ArrayAccess ? ($__internal_68aa442c1d43d3410ea8f958ba9090f3eaa9a76f8de8fc9be4d6c7389ba28002["#attributes"] ?? null) : null)) && is_array($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b) || $__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b instanceof ArrayAccess ? ($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b["class"] ?? null) : null));
+            foreach ($context['_seq'] as $context["key"] => $context["item"]) {
+                // line 33
+                echo "        ";
+                if (twig_in_filter("icon-", $context["item"])) {
+                    // line 34
+                    echo "          ";
+                    $context["item_id"] = twig_array_merge($this->sandbox->ensureToStringAllowed(($context["item_id"] ?? null), 34, $this->source), [0 => ("toolbar-id--" . $this->sandbox->ensureToStringAllowed($context["item"], 34, $this->source))]);
+                    // line 35
+                    echo "        ";
+                }
+                // line 36
+                echo "      ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['key'], $context['item'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 37
+            echo "
+      ";
+            // line 38
+            $context["tab_id"] = (((($__internal_d7fc55f1a54b629533d60b43063289db62e68921ee7a5f8de562bd9d4a2b7ad4 = twig_get_attribute($this->env, $this->source, $context["tab"], "link", [], "any", false, false, true, 38)) && is_array($__internal_d7fc55f1a54b629533d60b43063289db62e68921ee7a5f8de562bd9d4a2b7ad4) || $__internal_d7fc55f1a54b629533d60b43063289db62e68921ee7a5f8de562bd9d4a2b7ad4 instanceof ArrayAccess ? ($__internal_d7fc55f1a54b629533d60b43063289db62e68921ee7a5f8de562bd9d4a2b7ad4["#id"] ?? null) : null)) ? (("toolbar-tab--" . $this->sandbox->ensureToStringAllowed((($__internal_01476f8db28655ee4ee02ea2d17dd5a92599be76304f08cd8bc0e05aced30666 = twig_get_attribute($this->env, $this->source, $context["tab"], "link", [], "any", false, false, true, 38)) && is_array($__internal_01476f8db28655ee4ee02ea2d17dd5a92599be76304f08cd8bc0e05aced30666) || $__internal_01476f8db28655ee4ee02ea2d17dd5a92599be76304f08cd8bc0e05aced30666 instanceof ArrayAccess ? ($__internal_01476f8db28655ee4ee02ea2d17dd5a92599be76304f08cd8bc0e05aced30666["#id"] ?? null) : null), 38, $this->source))) : (""));
+            // line 39
+            echo "      ";
+            $context["tab_classes"] = twig_array_merge($this->sandbox->ensureToStringAllowed(($context["item_id"] ?? null), 39, $this->source), [0 => "toolbar-tab", 1 => ($context["user_menu"] ?? null), 2 => ($context["tab_id"] ?? null)]);
+            // line 40
+            echo "
+      ";
+            // line 41
+            $context["allowlist_items"] = [0 => "toolbar-id--toolbar-icon-escape-admin", 1 => "toolbar-id--toolbar-icon-shortcut", 2 => "toolbar-id--toolbar-icon-coffee", 3 => "toolbar-id--toolbar-icon-devel", 4 => "toolbar-id--toolbar-icon-rebuild-cache-access", 5 => "toolbar-id--toolbar-icon-user"];
+            // line 49
+            echo "
+      ";
+            // line 51
+            echo "      ";
+            if (twig_in_filter((($__internal_01c35b74bd85735098add188b3f8372ba465b232ab8298cb582c60f493d3c22e = ($context["item_id"] ?? null)) && is_array($__internal_01c35b74bd85735098add188b3f8372ba465b232ab8298cb582c60f493d3c22e) || $__internal_01c35b74bd85735098add188b3f8372ba465b232ab8298cb582c60f493d3c22e instanceof ArrayAccess ? ($__internal_01c35b74bd85735098add188b3f8372ba465b232ab8298cb582c60f493d3c22e[0] ?? null) : null), ($context["allowlist_items"] ?? null))) {
+                // line 52
+                echo "        <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["tab"], "attributes", [], "any", false, false, true, 52), "addClass", [0 => ($context["tab_classes"] ?? null)], "method", false, false, true, 52), 52, $this->source), "html", null, true);
+                echo ">
+          ";
+                // line 53
+                if ((((($__internal_63ad1f9a2bf4db4af64b010785e9665558fdcac0e8db8b5b413ed986c62dbb52 = ($context["item_id"] ?? null)) && is_array($__internal_63ad1f9a2bf4db4af64b010785e9665558fdcac0e8db8b5b413ed986c62dbb52) || $__internal_63ad1f9a2bf4db4af64b010785e9665558fdcac0e8db8b5b413ed986c62dbb52 instanceof ArrayAccess ? ($__internal_63ad1f9a2bf4db4af64b010785e9665558fdcac0e8db8b5b413ed986c62dbb52[0] ?? null) : null) == "toolbar-id--toolbar-icon-user") && ($context["user_picture"] ?? null))) {
+                    // line 54
+                    echo "            <a href=\"/user/";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["user"] ?? null), "id", [], "any", false, false, true, 54), 54, $this->source), "html", null, true);
+                    echo "\" class=\"toolbar-item icon-user\" role=\"button\" id=\"toolbar-item-user\">
+              <img src=\"";
+                    // line 55
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["user_picture"] ?? null), 55, $this->source), "html", null, true);
+                    echo "\" alt=\"";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["user"] ?? null), "account", [], "any", false, false, true, 55), "name", [], "any", false, false, true, 55), 55, $this->source), "html", null, true);
+                    echo "\" class=\"icon-user__image\" />
+            </a>
+          ";
+                } else {
+                    // line 58
+                    echo "            ";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["tab"], "link", [], "any", false, false, true, 58), 58, $this->source), "html", null, true);
+                    echo "
+          ";
+                }
+                // line 60
+                echo "          ";
+                ob_start(function () { return ''; });
+                // line 61
+                echo "            <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "attributes", [], "any", false, false, true, 61), 61, $this->source), "html", null, true);
+                echo ">
+              ";
+                // line 62
+                if (twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "label", [], "any", false, false, true, 62)) {
+                    // line 63
+                    echo "                <nav class=\"toolbar-lining clearfix\" role=\"navigation\" aria-label=\"";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "label", [], "any", false, false, true, 63), 63, $this->source), "html", null, true);
+                    echo "\">
+                  <h3 class=\"toolbar-tray-name visually-hidden\">";
+                    // line 64
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "label", [], "any", false, false, true, 64), 64, $this->source), "html", null, true);
+                    echo "</h3>
+              ";
+                } else {
+                    // line 66
+                    echo "                <nav class=\"toolbar-lining clearfix\" role=\"navigation\">
+              ";
+                }
+                // line 68
+                echo "              ";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["tray"] ?? null), "links", [], "any", false, false, true, 68), 68, $this->source), "html", null, true);
+                echo "
+              </nav>
+            </div>
+          ";
+                echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+                // line 72
+                echo "        </div>
+      ";
+            }
+            // line 74
+            echo "    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['key'], $context['tab'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 75
+        echo "  </nav>
+  ";
+        // line 76
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["remainder"] ?? null), 76, $this->source), "html", null, true);
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/gin/templates/navigation/toolbar--gin--secondary.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  178 => 76,  175 => 75,  169 => 74,  165 => 72,  157 => 68,  153 => 66,  148 => 64,  143 => 63,  141 => 62,  136 => 61,  133 => 60,  127 => 58,  119 => 55,  114 => 54,  112 => 53,  107 => 52,  104 => 51,  101 => 49,  99 => 41,  96 => 40,  93 => 39,  91 => 38,  88 => 37,  82 => 36,  79 => 35,  76 => 34,  73 => 33,  69 => 32,  66 => 31,  63 => 30,  60 => 29,  57 => 28,  53 => 27,  48 => 25,  44 => 24,  39 => 23,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/gin/templates/navigation/toolbar--gin--secondary.html.twig", "/var/www/html/web/themes/contrib/gin/templates/navigation/toolbar--gin--secondary.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 27, "set" => 28, "if" => 33, "spaceless" => 60);
+        static $filters = array("escape" => 23, "merge" => 34);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'set', 'if', 'spaceless'],
+                ['escape', 'merge'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
